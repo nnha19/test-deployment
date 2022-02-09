ac3d1aca5e165e9d78529a74ffd705e548ea1181
nnha19 <nyinyihmueaung19@gmail.com>
2022-02-09 17:16:54 +0630

    fake branch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
-      <p>Make some huge changes.</p>
-      <p>Netlify will no longer auto-deploy on pushing to github</p>
+      <p>Fake branch added on Netlify</p>
     </div>
   );
 };
